bdfcefe6d8b90057a4bb8b35fd88e35c6232a55b
Felix Geisendörfer <felix@debuggable.com>
2013-09-13 14:56:50 +0200

    Latest

--- bin/test.js
+++ bin/test.js
@@ -11,6 +11,7 @@ var async = require('async');
 var execFile = require('child_process').execFile;
 var Png = require('../lib/png');
 var Color = require('../lib/color');
+var detect = require('../lib/detect');
 
 async.waterfall([
   function read(next) {
@@ -27,10 +28,51 @@ async.waterfall([
       next(err, png);
     });
   },
-  function draw(png, next) {
+  function cells(png, next) {
     var start = Date.now();
+    var range = [
+      new Color(200, 50, 80),
+      new Color(255, 160, 180),
+    ];
+
+    //var range = [
+      //new Color(0, 0, 0),
+      //new Color(25, 25, 25),
+    //];
+
+    var redCells = [];
+    for (var x = 0; x < png.width(); x++) {
+      for (var y = 0; y < png.height(); y++) {
+        var color = png.get(x, y);
+        if (
+            color.red < range[0].red ||
+            color.red > range[1].red ||
+            color.green < range[0].green ||
+            color.green > range[1].green ||
+            color.blue < range[0].blue ||
+            color.blue > range[1].blue
+         ) {
+           continue;
+         }
+
+        redCells.push({x: x, y: y});
+      }
+    }
+    console.log('cells: %dms', Date.now()-start);
+    next(null, png, redCells);
+  },
+  function draw(png, redCells, next) {
+    var green = new Color(0, 255, 0);
+    //var skip = Math.ceil(redCells.length / 1000);
+    var skip = 1;
+
+    var start = Date.now();
+    for (var i = 0; i < redCells.length; i = i + skip) {
+      var cell = redCells[i];
+      png.set(cell.x, cell.y, new Color(255, 0, 0));
+    }
+    //png.line(249, 0, 267, 359, new Color(0, 255, 0), 20);
     console.log('draw: %dms', Date.now()-start);
-    png.line(10, 10, 100, 60, new Color(0, 255, 0), 10);
     next(null, png);
   },
   function encode(png, next) {
